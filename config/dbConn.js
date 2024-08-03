25fffe230614c210e4a16f930d29770018cbe598
ahmadifd <ahmadi.fd@gmail.com>
2024-08-03 13:37:31 +0330

    12312

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://127.0.0.1:27017/my-mern-food-ordering"
-      //"mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/node-app02"
+      //"mongodb://127.0.0.1:27017/my-mern-food-ordering"
+      "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/my-mern-food-ordering"
       );
   } catch (err) {
     console.log(err);
