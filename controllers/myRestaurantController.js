64254b5ab1dbb3e72d5169a115f324905182afa7
ahmadifd <ahmadi.fd@gmail.com>
2024-08-03 13:42:39 +0330

    34534

--- controllers/myRestaurantController.js
+++ controllers/myRestaurantController.js
@@ -37,69 +37,69 @@ const createRestaurant = async (req, res) => {
     });
   }
 
-  if (req.userId === undefined)
-    return res.status(400).json({
-      message: "Error userId",
-    });
+  // if (req.userId === undefined)
+  //   return res.status(400).json({
+  //     message: "Error userId",
+  //   });
 
-  const userId = req.userId;
+  // const userId = req.userId;
 
-  try {
-    const existingRestaurant = await Restaurant.findOne({ user: userId });
-    if (existingRestaurant) {
-      return res
-        .status(409)
-        .json({ message: "User restaurant already exists" });
-    }
+  // try {
+  //   const existingRestaurant = await Restaurant.findOne({ user: userId });
+  //   if (existingRestaurant) {
+  //     return res
+  //       .status(409)
+  //       .json({ message: "User restaurant already exists" });
+  //   }
 
-    const {
-      restaurantName,
-      city,
-      country,
-      deliveryPrice,
-      estimatedDeliveryTime,
-      cuisines,
-      menuItems,
-    } = req.body;
+  //   const {
+  //     restaurantName,
+  //     city,
+  //     country,
+  //     deliveryPrice,
+  //     estimatedDeliveryTime,
+  //     cuisines,
+  //     menuItems,
+  //   } = req.body;
 
-    const file = req.files.imageFile[0];
-    const fsPromises = fs.promises;
-    const filename = "imageFile-" + Date.now();
-    const extname = file.originalname.slice(
-      file.originalname.lastIndexOf(".") + 1
-    );
-    const filefullname = filename + "." + extname;
+  //   const file = req.files.imageFile[0];
+  //   const fsPromises = fs.promises;
+  //   const filename = "imageFile-" + Date.now();
+  //   const extname = file.originalname.slice(
+  //     file.originalname.lastIndexOf(".") + 1
+  //   );
+  //   const filefullname = filename + "." + extname;
 
-    await fsPromises.writeFile(
-      path.join("./public/uploads", filefullname),
-      file.buffer
-    );
+  //   await fsPromises.writeFile(
+  //     path.join("./public/uploads", filefullname),
+  //     file.buffer
+  //   );
 
-    const restaurant = new Restaurant();
-    restaurant.restaurantName = restaurantName;
-    restaurant.city = city;
-    restaurant.country = country;
-    restaurant.deliveryPrice = Number(deliveryPrice);
-    restaurant.estimatedDeliveryTime = Number(estimatedDeliveryTime);
+  //   const restaurant = new Restaurant();
+  //   restaurant.restaurantName = restaurantName;
+  //   restaurant.city = city;
+  //   restaurant.country = country;
+  //   restaurant.deliveryPrice = Number(deliveryPrice);
+  //   restaurant.estimatedDeliveryTime = Number(estimatedDeliveryTime);
 
-    restaurant.cuisines = cuisines;
-    restaurant.menuItems = menuItems.map((item) => ({
-      ...item,
-      _id: mongoose.Types.ObjectId(),
-    }));
+  //   restaurant.cuisines = cuisines;
+  //   restaurant.menuItems = menuItems.map((item) => ({
+  //     ...item,
+  //     _id: mongoose.Types.ObjectId(),
+  //   }));
 
-    restaurant.imageUrl = filefullname;
-    restaurant.user = new mongoose.Types.ObjectId(req.userId);
-    restaurant.lastUpdated = new Date();
+  //   restaurant.imageUrl = filefullname;
+  //   restaurant.user = new mongoose.Types.ObjectId(req.userId);
+  //   restaurant.lastUpdated = new Date();
 
-    await restaurant.save();
-    restaurant.imageUrl =
-      process.env.VITE_API_BASE_URL + "//uploads/" + restaurant.imageUrl;
-    res.status(201).json(restaurant);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
+  //   await restaurant.save();
+  //   restaurant.imageUrl =
+  //     process.env.VITE_API_BASE_URL + "//uploads/" + restaurant.imageUrl;
+  //   res.status(201).json(restaurant);
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).json({ message: "Something went wrong" });
+  // }
 };
 
 const editRestaurant = async (req, res) => {
