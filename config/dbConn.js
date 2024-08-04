3a417e070f6ecd8a02ff70f0200641938a196e7c
ahmadifd <ahmadi.fd@gmail.com>
2024-08-04 12:02:21 +0330

    23423

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      //"mongodb://127.0.0.1:27017/my-mern-food-ordering"
-      "mongodb+srv://ahmadifd:47mcOiksjZjyvH1Y@cluster0.jad41dh.mongodb.net/my-mern-food-ordering"
-      );
+    await mongoose.connect(process.env.CONNECTION_STRING);
   } catch (err) {
     console.log(err);
   }
