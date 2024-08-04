8c38a84ade528a19fd1020709dcca0506fac1f2c
ahmadifd <ahmadi.fd@gmail.com>
2024-08-04 10:43:34 +0330

    34534

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,5 @@ export const allowedOrigins = [
   "http://localhost:5075",
   "http://localhost:5076",
   "http://localhost:3500",
-  "https://my-mern-frontend.netlify.app",
+  "https://fdahmadi.netlify.app",
 ];
