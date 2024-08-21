a5e4d4d1ad986e6ce8670fa12a997475dcccccb7
ahmadifd <ahmadi.fd@gmail.com>
2024-08-21 11:43:33 +0330

    23452345

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,4 +3,6 @@ export const allowedOrigins = [
   "http://localhost:5076",
   "http://localhost:3500",
   "https://fdahmadi.netlify.app",
+  "http://localhost:1000",
+  "http://91.247.171.34:1000",
 ];
